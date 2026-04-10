98f0f5814d18ca2dfd451c6521cb1ee01cbee786
Jkreis2019 <jkreis2019@gmail.com>
2026-04-10 15:20:15 -0700

    Block react-native-maps on web via metro resolver

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,15 @@ const config = getDefaultConfig(__dirname)
 
 config.resolver.platforms = ['ios', 'android', 'web']
 
+const originalResolver = config.resolver.resolveRequest
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (platform === 'web' && moduleName === 'react-native-maps') {
+    return { type: 'empty' }
+  }
+  if (originalResolver) {
+    return originalResolver(context, moduleName, platform)
+  }
+  return context.resolveRequest(context, moduleName, platform)
+}
+
 module.exports = config
